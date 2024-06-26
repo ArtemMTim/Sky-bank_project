119427b889bdcb9270128e39b4c36450d66681c8
Timofeev Artem <artemtimofeev@rambler.ru>
2024-06-26 19:45:53 +0300

    Добавлена возможность логгирования для masks.py и utils.py. Дополнен README файл.

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,38 @@
+import logging
+import os
 from typing import Union
+
+# Определяем путь к папке "logs" для различных ОС
+if os.name == "nt":
+    project_path = "\\".join((os.path.dirname(os.path.abspath(__file__))).split("\\")[:-1])
+    log_path = f"{project_path}\\logs\\"
+else:
+    project_path = "/".join((os.path.dirname(os.path.abspath(__file__))).split("/")[:-1])
+    log_path = f"{project_path}/logs/"
+
+# Проводим настройку логгеров для логирования в файл (уровень DEBUG) и в консоль (уровень ERROR)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s",
+    filename=f"{log_path}masks.log",
+    filemode="w",
+)
+
+# Определяем логгеры для каждой из представленных функций
+card_logger = logging.getLogger("app.credit_card")
+bank_logger = logging.getLogger("app.bank_account")
 
 
 def credit_card_masking(number: Union[int, str]) -> str:
     """Функция возвращает маску номера кредитной карты по шаблону - 'ХХХХ ХХ** **** ХХХХ'"""
     # Проверяем на корректность введённый номер карты. Возвращаем предупреждение при непрохождении проверки.
     # В противном случае возвращаем маску.
+    card_logger.info("Программа  начала работу.")
     if str(number).isdigit() and len(str(number)) == 16:
+        card_logger.info("Программа успешно завершила свою работу.")
         return f"{str(number)[0:4]} {str(number)[4:6]}** **** {str(number)[-4:]}"
     else:
+        card_logger.error("Введён не корректный номер кредитной карты!")
         return "Введён не корректный номер кредитной карты! Проверьте!"
 
 
@@ -15,7 +40,10 @@ def bank_account_masking(number: Union[int, str]) -> str:
     """Функция возвращает маску номера банковского счёта по шаблону - '**ХХХХ'"""
     # Проверяем на корректность введённый номер счёта. Возвращаем предупреждение при непрохождении проверки.
     # В противном случае возвращаем маску.
+    bank_logger.info("Программа начала работу.")
     if str(number).isdigit() and len(str(number)) == 20:
+        bank_logger.info("Программа успешно завершила свою работу.")
         return f"**{str(number)[-4:]}"
     else:
+        bank_logger.error("Введён не корректный номер банковского счёта!")
         return "Введён не корректный номер банковского счёта! Проверьте!"
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,34 @@
 import json
+import logging
 import os
 from json import JSONDecodeError
 from typing import Dict, List, Union
+
+# Определяем путь к папке "logs" для различных ОС
+if os.name == "nt":
+    project_path = "\\".join((os.path.dirname(os.path.abspath(__file__))).split("\\")[:-1])
+    log_path = f"{project_path}\\logs\\"
+else:
+    project_path = "/".join((os.path.dirname(os.path.abspath(__file__))).split("/")[:-1])
+    log_path = f"{project_path}/logs/"
+
+# Проводим настройку логгеров для логирования в файл (уровень DEBUG) и в консоль (уровень ERROR)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s",
+    filename=f"{log_path}utils.log",
+    filemode="w",
+)
+
+# Определяем логгеры для каждой из представленных функций
+transact_logger = logging.getLogger("app.transact")
 
 
 def transactions_list_func(file_name: str) -> List[Dict[str, Union[str, int, dict, float]]]:
     """Принимает название JSON файла с информацией о транзакциях.
     Файл располагается в директории data корневого каталого проекта.
     Возвращает список словарей транзакций."""
+    transact_logger.info("Программа начинает работу.")
     if os.name == "nt":
         project_path = "\\".join((os.path.dirname(os.path.abspath(__file__))).split("\\")[:-1])
         data_path = f"{project_path}\\data\\"
@@ -15,16 +36,23 @@ def transactions_list_func(file_name: str) -> List[Dict[str, Union[str, int, dic
         project_path = "/".join((os.path.dirname(os.path.abspath(__file__))).split("/")[:-1])
         data_path = f"{project_path}/data/"
     try:
+        transact_logger.info(f"Программа пытается открыть файл '{file_name}'.")
         with open(f"{data_path}{file_name}", "r", encoding="UTF-8") as file:
+            transact_logger.info(f"Программа начинает обработку файла '{file_name}'.")
             try:
                 data = json.load(file)
+                transact_logger.info(f"Программа успешно обработала файл '{file_name}'.")
                 if isinstance(data, list):
+                    transact_logger.info("Программа завершает свою работу.")
                     return data
                 else:
+                    transact_logger.info("Программа завершает свою работу.")
                     return []
             except JSONDecodeError:
                 print("Невозможно декодировать файл. Проверьте содержимое файла!")
+                transact_logger.error("Невозможно декодировать файл!'.")
                 return []
     except FileNotFoundError:
         print("Указанный файл не найден!")
+        transact_logger.error("Файл не найден!")
         return []

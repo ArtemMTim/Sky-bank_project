689c4800449badb4115b35dfd1ffef3eb9034a94
Timofeev Artem <artemtimofeev@rambler.ru>
2024-07-04 18:58:01 +0300

    Разработаны тесты модуля обработки csv и excel файлов с применением mock и patch

--- tests/test_csv_excel_reader.py
+++ tests/test_csv_excel_reader.py
@@ -1,41 +1,58 @@
 import pytest
 from src.csv_excel_reader import csv_excel_reader
-from unittest.mock import patch, mock_open
+from unittest.mock import patch
 
 
-
-expected = [
-    {
-        "id": 650703.0,
-        "state": "EXECUTED",
-        "date": "2023-09-05T11:30:32Z",
-        "amount": 16210.0,
-        "currency_name": "Sol",
-        "currency_code": "PEN",
-        "from": "Счет 58803664561298323391",
-        "to": "Счет 39745660563456619397",
-        "description": "Перевод организации",
-    },
-    {
-        "id": 3598919.0,
-        "state": "EXECUTED",
-        "date": "2020-12-06T23:00:58Z",
-        "amount": 29740.0,
-        "currency_name": "Peso",
-        "currency_code": "COP",
-        "from": "Discover 3172601889670065",
-        "to": "Discover 0720428384694643",
-        "description": "Перевод с карты на карту",
-    },
-]
+# Тестирование с mock patch
+# Тестирование считывания csv файла
+@patch("pandas.read_csv")
+def test_csv_reader(mock_read_csv):
+    mock_read_csv.return_value.to_dict.return_value = [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+        }
+    ]
+    result = csv_excel_reader("test_file.csv")
+    assert result == [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+        }
+    ]
 
 
-# Тестируем работу модуля с файлами csv, xlsx
-@pytest.mark.parametrize("file_name, expected", [("test_1.csv", expected), ("test_2.xlsx", expected)])
-def test_csv_excel_reader(file_name, expected):
-    assert csv_excel_reader(file_name) == expected
+# Тестирование считывания xlsx файла
+@patch("pandas.read_excel")
+def test_excel_reader(mock_read_excel):
+    mock_read_excel.return_value.to_dict.return_value = [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+        }
+    ]
+    result = csv_excel_reader("test_file.xlsx")
+    assert result == [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+        }
+    ]
 
 
+# Тестирование выдачи ошибок
 # Тестируем выдачу ошибки о неподдерживаемом формате файла
 def test_csv_excel_reader_with_wrong_format_file():
     with pytest.raises(ValueError) as exc_info:
@@ -48,22 +65,3 @@ def test_csv_excel_reader_with_no_file():
     with pytest.raises(Exception) as exc_info:
         csv_excel_reader("test_none.csv")
         assert str(exc_info.value) == "При считывании файла произошла ошибка."
-
-
-#Тестирование с mock patch
-data = "[{'id': 650703.0, 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': 16210.0, 'currency_name': 'Sol'}]"
-expected_result = [
-        {'id': 650703.0, 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': 16210.0, 'currency_name': 'Sol'}
-    ]
-
-# При включении теста появляется assertion error - пустой список не равен ожидаемому
-@patch("builtins.open", new_callable=mock_open, read_data=data)
-def test_csv_excel_reader_2(mock_file):
-    result = csv_excel_reader("test_file.csv")
-    assert result == expected_result
-
-# При включении теста появляется assertion error - None не равен ожидаемому
-def test_csv_excel_reader_3():
-    with patch('builtins.open') as mock_open:
-        mock_open = [{'id': 650703.0, 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': 16210.0, 'currency_name': 'Sol'}]
-        assert csv_excel_reader("test_file.csv") == expected_result
